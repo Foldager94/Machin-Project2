0cfcc5664bb3f3cb195f85e86e1149c3a5e96838
sand4570 <sand4570@stud.kea.dk>
2023-05-05 08:41:33 +0200

    make logic and validation for moves

--- project/core/src/card.h
+++ project/core/src/card.h
@@ -4,14 +4,6 @@
 #include <stdbool.h>
 
 extern const int CARD_A;
-extern const int CARD_2;
-extern const int CARD_3;
-extern const int CARD_4;
-extern const int CARD_5;
-extern const int CARD_6;
-extern const int CARD_7;
-extern const int CARD_8;
-extern const int CARD_9;
 extern const int CARD_T;
 extern const int CARD_J;
 extern const int CARD_Q;
@@ -30,5 +22,7 @@ extern void insert_list_in_list(Card* dummy, Card* first, Card* last);
 extern Card* pop_last_in_list(Card* dummy);
 extern void clear_list(Card* dummy);
 extern Card* init_list();
+extern int ASCII_to_numeric(int cardValue);
+extern Card* find_card(Card* dummy, char thisCardSuit, char thisCardValue);
 
 #endif
