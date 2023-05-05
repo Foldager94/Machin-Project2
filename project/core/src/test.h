3166d8f164949f5dfcbea5967ec01c32073a62f4
Christoffer <c.k.foldager@gmail.com>
2023-05-05 19:35:53 +0200

    merge test

---
+++ project/core/src/test.h
@@ -0,0 +1,13 @@
+
+
+#ifndef MACHIN_PROJECT2_TEST_H
+#define MACHIN_PROJECT2_TEST_H
+
+extern void testLoadFile();
+extern void testCreateDeck();
+extern void testShuffleDeck();
+extern void testShuffleDeckRandom();
+extern void testASCII_to_numeric();
+extern void runTests();
+
+#endif //MACHIN_PROJECT2_TEST_H
